52479657402a0041080675ef9c023a0123089e9c
gaurav876409 <gaurav876409@gmail.com>
2023-07-25 09:01:12 +0530

    up

--- models/user.js
+++ models/user.js
@@ -1,5 +1,23 @@
 var mongoose= require("mongoose");
 
 //Write your schema here with name of schema as userSchema
+const userSchema = {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    pswd: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ['buyer', 'seller'],
+    },
+  };
 
 module.exports =mongoose.model("users",userSchema);
